3d5ab1b540044f213ff65bb8f44d0e9b08ec4a96
J4Joshua <joshhhht002@gmail.com>
2023-05-11 15:36:18 +0800

    test

---
+++ app/backend.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/data')
+def get_data():
+    # Connect to the MySQL DB instance
+    cnx = mysql.connector.connect(user='admin', password='Arby_13245',
+                                   host='arbydb.camodfosky75.ap-southeast-1.rds.amazonaws.com', database='ARBY')
+    
+    # Execute an SQL query to retrieve data from the table
+    cursor = cnx.cursor()
+    query = 'SELECT * FROM Tokens'
+    cursor.execute(query)
+    data = cursor.fetchall()
+    
+    # Close the database connection
+    cnx.close()
+    print(data)
+    
+    # Return the retrieved data as a JSON response
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run()
+
